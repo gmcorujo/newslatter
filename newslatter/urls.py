2a493edf9f65c243293a14a446f24e61074f185a
Corujo Gustavo Martín <v8martincoru@gmail.com>
2024-02-19 16:09:38 -0300

    add template and change views

---
+++ newslatter/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+
+from .views import newslatter_signup
+
+urlpatterns = [
+    #path('', newslatter_signup, name='newslatter_signup'),
+]
--- newslatter/views.py
+++ newslatter/views.py
@@ -1,3 +1,29 @@
+from email import message
 from django.shortcuts import render
 
-# Create your views here.
+from .forms import NewslatterUserSingUpForm
+from .models import NewslatterUser
+
+
+def newslatter_signup(request):
+    form = NewslatterUserSingUpForm(request.POST or None)
+
+    if form.is_valid():
+        instance = form.save(commit=False)
+        if NewslatterUser.objects.filter(email=instance.email).exists():
+            message.warning(request, 'Email already exists')
+
+        else:
+            instance.save()
+            message.success(request, 'Thanks for signing up, check your email')
+            # send email                 
+            subject = 'Registrado a Newslatter de la Empresa'
+            body = 'Gracias por registrarte en Newslatter de la Empresa'
+            from_email = instance.email
+            to_email = [instance.email]
+        
+    context = {
+        'form': form
+    }
+    return render(request, 'newslatter/start_here.html', context)
+        
