6746180d9ae5c4f359feafb8ffa5ed15d92186b3
Martín Corujo <v8martincoru@gmail.com>
2024-11-16 01:54:42 -0300

    register admin models

--- newslatter/admin.py
+++ newslatter/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Newslatter, NewslatterUser
+
+
+admin.site.register(Newslatter)
+admin.site.register(NewslatterUser)
